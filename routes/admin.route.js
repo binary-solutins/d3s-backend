f3c641320101f8f5abd8513d9f8f12f01e803255
satweekg <satweekg.shivinfotech@gmail.com>
2025-05-05 11:39:04 +0530

    admin routes correcrtion

--- routes/admin.route.js
+++ routes/admin.route.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const router = express.Router();
 const adminController = require('../controllers/admin.controller');
-const authMiddleware = require('../middleware/auth');
+const authMiddleware = require('../middlewares/auth');
 
 /**
  * @swagger
